5a7261b16c30fcb4c4b0b7a733f9d62ececed6c5
polosjacket <53041211+polosjacket@users.noreply.github.com>
2026-04-19 12:38:23 +0800

    make some changes

--- public/game.js
+++ public/game.js
@@ -134,6 +134,21 @@ class SoundEffects {
         });
     }
 
+    playPowerUp() {
+        if (!this.ctx) return;
+        const osc = this.ctx.createOscillator();
+        const gain = this.ctx.createGain();
+        osc.type = 'sine';
+        osc.frequency.setValueAtTime(400, this.ctx.currentTime);
+        osc.frequency.exponentialRampToValueAtTime(800, this.ctx.currentTime + 0.2);
+        gain.gain.setValueAtTime(0.05, this.ctx.currentTime);
+        gain.gain.exponentialRampToValueAtTime(0.01, this.ctx.currentTime + 0.2);
+        osc.connect(gain);
+        gain.connect(this.ctx.destination);
+        osc.start();
+        osc.stop(this.ctx.currentTime + 0.2);
+    }
+
     playIntroBGM() {
         if (!this.ctx || this.bgmOsc) return;
         const tempo = 80;
